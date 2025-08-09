4bb77e060fae965f8cb1c2e14414d5d7f7a0805c
vinodh2302 <vinodh2302@gmail.com>
2025-08-09 20:46:12 +0530

    Corrected wmsreceiver application

--- wmsreceiverapplication/Worker.cs
+++ wmsreceiverapplication/Worker.cs
@@ -1,5 +1,8 @@
 using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
 using System.Text;
+using System.Text.Json;
+using WMSSystems.Models;
 
 namespace wmsreceiverapplication
 {
@@ -26,22 +29,43 @@ namespace wmsreceiverapplication
             //    UserName = "guest",
             //    Password = "guest"
             //};
-
             var factory = new ConnectionFactory
             {
-                HostName = Environment.GetEnvironmentVariable("RABBITMQ__HOSTNAME") ?? "localhost",
-                UserName = Environment.GetEnvironmentVariable("RABBITMQ__USERNAME") ?? "guest",
-                Password = Environment.GetEnvironmentVariable("RABBITMQ__PASSWORD") ?? "guest"
+                Port = 5672,
+                HostName = "rabbitmq",
+                UserName =  "guest",
+                Password = "guest"
             };
 
-            _connection = factory.CreateConnection();
-            _channel = _connection.CreateModel();
+            IConnection connection = null;
+            int retries = 10;
+            while (retries > 0)
+            {
+                try
+                {
+                    connection = factory.CreateConnection();
+                    Console.WriteLine("Connected to RabbitMQ!");
+                    break;
+                }
+                catch (BrokerUnreachableException)
+                {
+                    retries--;
+                    Console.WriteLine("RabbitMQ not ready, retrying...");
+                    Thread.Sleep(5000);
+                }
+            }
+            _logger.LogInformation("Came 12");
+            if(connection != null)
+            {
+                _channel = connection.CreateModel();
 
-            _channel.QueueDeclare(queue: QueueName,
-                                 durable: true,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
+                _channel.QueueDeclare(queue: QueueName,
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+            }
+           
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -53,11 +77,23 @@ namespace wmsreceiverapplication
                 {
                     var message = Encoding.UTF8.GetString(result.Body.ToArray());
                     _logger.LogInformation($"Fetched: {message}");
+                    var customer = JsonSerializer.Deserialize<Customer>(message);
 
-                    var content = new StringContent(message, Encoding.UTF8, "application/json");
+                    if (customer == null)
+                    {
+                        _logger.LogWarning("Received message could not be deserialized to Product.");
+                        continue;
+                    }
+                    var content = new StringContent(
+                       JsonSerializer.Serialize(customer),
+                       Encoding.UTF8,
+                       "application/json"
+                     );
+
+                    //var content = new StringContent(message, Encoding.UTF8, "application/json");
                     try
                     {
-                        var response = await _httpClient.PostAsync("http://your-api:5000/api/messages", content, stoppingToken);
+                        var response = await _httpClient.PostAsync("http://wmsapi:80/api/Customer/customers", content, stoppingToken);
                         if (response.IsSuccessStatusCode)
                         {
                             _logger.LogInformation("Message forwarded successfully.");
